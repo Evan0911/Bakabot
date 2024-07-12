6fb7d90df1c7ef6895225b87b8bdf15a82247adf
Evan <63302524+Evan0911@users.noreply.github.com>
2024-07-12 08:04:42 +0200

    add wires to events

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,5 +11,9 @@ module.exports = {
     if (likeARegex.test(message.content)) {
       message.reply({content: "Like a what ?", allowedMentions: {repliedUser: false}});
     }
+    const wiresRegex = new RegExp(/wires|wire/gmi);
+    if (wiresRegex.test(message.content)) {
+      message.reply({content: "https://static.wikia.nocookie.net/silly-cat/images/4/4f/Wire_Cat.png/revision/latest/thumbnail/width/360/height/360?cb=20231001190626", allowedMentions: {repliedUser: false}});
+    }
   },
 };
